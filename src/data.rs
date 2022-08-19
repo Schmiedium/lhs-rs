96133a8887ff36656da49085af672925f29f59be
Schmiedium <alexander.eisenschmied@gmail.com>
2022-08-18 23:21:08 -0400

    broke out the data range and sample space stuff to it's own file. started documenting code with markdown comments

---
+++ src/data.rs
@@ -0,0 +1,69 @@
+pub mod Data {
+    use serde_json;
+    use std;
+    use serde_json::Value;
+
+    pub(crate) struct DataRange {
+        pub(crate) name: String,
+        pub(crate) lower_bound: f64,
+        pub(crate) upper_bound: f64
+    }
+
+    impl DataRange {
+        pub(crate) fn new(range: Value) -> std::result::Result<DataRange, &'static str>{
+
+            let mut name: String = String::new();
+            let lower_bound: f64;
+            let upper_bound: f64;
+
+            match range["name"].as_str() {
+                Some(x) => name = x.to_string(),
+                None => return Err("all variables must specify a name"),
+            };
+            match range["lower_bound"].as_f64() {
+                Some(x) => lower_bound = x,
+                None => return Err("all variables must specify a lower bound"),
+            };
+            match range["upper_bound"].as_f64() {
+                Some(x) => upper_bound = x,
+                None => return Err("all variables must specify an upper bound"),
+            };
+
+            Ok(DataRange{ name: name, lower_bound: lower_bound, upper_bound: upper_bound})
+        }
+    }
+
+    pub struct SampleSpace {
+        pub(crate) space: Vec<DataRange>,
+        pub(crate) samples: i64,
+    }
+
+    impl SampleSpace {
+        pub fn new(input: &str) -> std::result::Result<SampleSpace, &'static str> {
+            let json_input: serde_json::Value = serde_json::from_str(&input).expect("unable to read file");
+
+            let samples: i64;
+            match json_input["samples"].as_i64() {
+                Some(x) => (samples = x),
+                None => return Err("json file must contain a \"samples\" field"),
+            };
+            let array = json_input["parameters"].as_array();
+            let mut data: Vec<Value> = Vec::new();
+            let mut results: Vec<DataRange> = Vec::new();
+
+        
+
+            match array {
+                Some(x) => data = x.to_vec(),
+                None => return Err("input file not structured properly"),
+            }
+
+            for v in data {
+                let range = DataRange::new(v)?;
+                results.push(range);
+            }
+
+            Ok(SampleSpace{ space: results, samples: samples })
+        }
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
-use std::{fs, error::Error, result, process::Output};
-use serde_json::{Value};
 use itertools::Itertools;
-use rand::{seq::{IteratorRandom, SliceRandom}, Rng};
+use rand::seq::SliceRandom;
+use std::{collections::HashSet, error::Error, fs, hash::Hash};
+mod data;
+use crate::data::Data::SampleSpace;
 
+///Main logic function. main handles arguments and calls run
+/// 
+/// # Arguments
+/// * Accepts a reference to a string slice containing the file path to the json input file
+/// 
+/// # Returns
+/// * Result type containing either nothing or an errror with what went wrong
 pub fn run(input: &str) -> std::result::Result<(), Box<dyn Error>> {
     // println!("{}", input);
     let contents: String = fs::read_to_string(input)?;
@@ -16,137 +24,81 @@ pub fn run(input: &str) -> std::result::Result<(), Box<dyn Error>> {
 
 
 
-struct DataRange {
-    name: String,
-    lower_bound: f64,
-    upper_bound: f64
-}
-
-impl DataRange {
-    fn new(range: Value) -> std::result::Result<DataRange, &'static str>{
-
-        let mut name: String = String::new();
-        let lower_bound: f64;
-        let upper_bound: f64;
-
-        match range["name"].as_str() {
-            Some(x) => name = x.to_string(),
-            None => return Err("all variables must specify a name"),
-        };
-        match range["lower_bound"].as_f64() {
-            Some(x) => lower_bound = x,
-            None => return Err("all variables must specify a lower bound"),
-        };
-        match range["upper_bound"].as_f64() {
-            Some(x) => upper_bound = x,
-            None => return Err("all variables must specify an upper bound"),
-        };
-
-        Ok(DataRange{ name: name, lower_bound: lower_bound, upper_bound: upper_bound})
-    }
-}
-
-pub struct SampleSpace {
-    space: Vec<DataRange>,
-    samples: i64,
-}
-
-impl SampleSpace {
-    pub fn new(input: &str) -> std::result::Result<SampleSpace, &'static str> {
-        let json_input: serde_json::Value = serde_json::from_str(&input).expect("unable to read file");
-
-        let samples: i64;
-        match json_input["samples"].as_i64() {
-            Some(x) => (samples = x),
-            None => return Err("json file must contain a \"samples\" field"),
-        };
-        let array = json_input["parameters"].as_array();
-        let mut data: Vec<Value> = Vec::new();
-        let mut results: Vec<DataRange> = Vec::new();
-
-        
-
-        match array {
-            Some(x) => data = x.to_vec(),
-            None => return Err("input file not structured properly"),
-        }
-
-        for v in data {
-            let range = DataRange::new(v)?;
-            results.push(range);
-        }
-
-        Ok(SampleSpace{ space: results, samples: samples })
-    }
-}
-
 fn generate_levels(samples: i64) -> Vec<i64> {
-        
     let mut strata = Vec::new();
 
-    (-samples/2..(samples/2)+1).for_each(|i: i64| {
+    (-samples / 2..(samples / 2) + 1).for_each(|i: i64| {
         strata.push(i);
     });
 
     strata
 }
 
-
-fn generate_level_perms(mut levels: Vec<i64>, factors: usize) -> Result< Vec<Vec<i64>>, &'static str> {
-    let len = levels.len();
-    if len < factors {
-        return Err("invalid space: must have more levels than factors to test");
-    }
-
+fn generate_level_perms(
+    mut levels: Vec<i64>,
+    factors: usize,
+) -> Result<Vec<Vec<i64>>, &'static str> {
     let mut range = rand::thread_rng();
 
-    Ok((0..factors).into_iter().map(|_| -> Vec<i64> {
-
-        levels.shuffle(&mut range);
-        levels.iter().map(|x| -> i64 {*x}).collect_vec()
-
-        // let n: usize = range.gen_range(0..factorial(len));
-        // let vec = levels.iter().permutations(len).nth(n).unwrap();
-        // return vec.into_iter().map(|x| -> i64 {*x}).collect_vec();
-    }).collect_vec())
+    Ok((0..factors)
+        .into_iter()
+        .map(|_| -> Vec<i64> {
+            levels.shuffle(&mut range);
+            levels.iter().map(|x| -> i64 { *x }).collect_vec()
+        })
+        .collect_vec())
 }
 
-fn generate_sample_matrix(space: SampleSpace) -> Result< Vec<Vec<f64>>, &'static str>{
-
+fn generate_sample_matrix(space: SampleSpace) -> Result<Vec<Vec<f64>>, &'static str> {
     let samples = space.samples;
     let factors = space.space.len();
 
     let levels = generate_levels(samples);
     let level_matrix = generate_level_perms(levels, factors)?;
 
-    Ok(level_matrix.into_iter()
-    .map(|column: Vec<i64>| -> Vec<f64> {
-
-        column.into_iter()
-        .map(|entry| -> f64 {
-            let random = rand::random::<f64>();
-            let entry = (entry + (samples-1)/2) as f64;
-            (entry + random)/(samples as f64)
+    Ok(level_matrix
+        .into_iter()
+        .map(|column: Vec<i64>| -> Vec<f64> {
+            column
+                .into_iter()
+                .map(|entry| -> f64 {
+                    let random = rand::random::<f64>();
+                    let entry = (entry + (samples - 1) / 2) as f64;
+                    (entry + random) / (samples as f64)
+                })
+                .collect_vec()
         })
-        .collect_vec()
-
-    }).collect_vec())
-
+        .collect_vec())
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn has_unique_elements<T>(iter: T) -> bool
+    where
+        T: IntoIterator,
+        T::Item: Eq + Hash,
+    {
+        let mut uniq = HashSet::new();
+        iter.into_iter().all(move |x| uniq.insert(x))
+    }
+
     #[test]
     fn test_levels_output() {
         assert_eq!(generate_levels(5), vec![-2, -1, 0, 1, 2]);
     }
-    
-    
+
     #[test]
     fn test_level_columns() {
         assert_eq!(generate_level_perms(vec![-1, 0, 1], 2).unwrap().len(), 2)
+    }
+
+    #[test]
+    fn test_level_columns_unique() {
+        assert!(has_unique_elements(generate_level_perms(
+            generate_levels(100),
+            4
+        )))
     }
 }
